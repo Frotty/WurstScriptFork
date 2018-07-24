5338b2cd7644d9524f6b068594ab9a4f28ef30b0
Frotty <frotty@sunayama.de>
2018-07-24 11:28:15 +0200

    shuffle global vars

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/jassprinter/JassPrinter.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/jassprinter/JassPrinter.java
@@ -7,6 +7,7 @@ import de.peeeq.wurstio.gui.AboutDialog;
 import de.peeeq.wurstscript.jassAst.*;
 import de.peeeq.wurstscript.utils.Utils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -28,7 +29,8 @@ public class JassPrinter {
         Preconditions.checkNotNull(sb);
         Preconditions.checkNotNull(prog);
 
-        sb.append(WURST_COMMENT + "\n");
+        Collections.shuffle(prog.getGlobals());
+
         printTypes(sb, prog.getDefs());
         printGlobals(sb, prog.getGlobals());
         printNatives(sb, prog.getNatives());
