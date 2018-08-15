6fc076c27fb2c7ce97752359a25f7bb1a440040c
Frotty <frotty@sunayama.de>
2018-08-15 23:06:46 +0200

    experimental reun

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/WurstCompilerJassImpl.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/WurstCompilerJassImpl.java
@@ -422,6 +422,15 @@ public class WurstCompilerJassImpl implements WurstCompiler {
             optimizer.localOptimizations();
         }
 
+        // inliner
+        if (runArgs.isInline()) {
+            beginPhase(11, "inlining");
+            optimizer.doInlining();
+            imTranslator2.assertProperties();
+
+            printDebugImProg("./test-output/im " + stage++ + "_afterinline.im");
+        }
+
         printDebugImProg("./test-output/im " + stage++ + "_afterlocalopts.im");
 
         if (runArgs.isNullsetting()) {
