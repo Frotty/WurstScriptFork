0ec099931034a68e86ec0b133f653a68838e582c
Frotty <Frotty@users.noreply.github.com>
2021-06-28 20:41:37 +0200

    Update StringCryptor.java

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/translation/imoptimizer/StringCryptor.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/translation/imoptimizer/StringCryptor.java
@@ -13,15 +13,15 @@ import de.peeeq.wurstscript.validation.TRVEHelper;
 import org.apache.commons.lang.math.RandomUtils;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StringCryptor {
-
-    public void encrypt(ImTranslator trans) {
+    public static HashSet<String> forbidden = new HashSet<>();
+    static {
+        forbidden.add("TRIGSTR");
+    }
+    public static void encrypt(ImTranslator trans) {
         ImProg prog = trans.getImProg();
         int[] key = new int[]{25, 12, 33, 17};
         Optional<ImFunction> decryptFunc = prog.getFunctions().stream().filter(f -> f.getName().contains("w3pro_decrypt_string")).findFirst();
@@ -30,30 +30,34 @@ public class StringCryptor {
             prog.accept(new ImProg.DefaultVisitor() {
                 @Override
                 public void visit(ImStringVal stringVal) {
-                    if (stringVal.getValS().startsWith("w3pro_")) {
-                        stringVal.replaceBy(JassIm.ImStringVal(stringVal.getValS().substring(6)));
-                    } else {
-                        String text = stringVal.getValS();
-                        StringBuilder crypted = new StringBuilder();
-                        int i = 0;
-                        for (char c : text.toCharArray()) {
-                            if (c > 31 && c < 127) {
-                                char shifted = (char)(c - ((char) key[i]));
-                                if (shifted < 32) {
-                                    shifted += (128 - 32);
+                    Optional<String> forbidden = StringCryptor.forbidden.stream().filter(f -> stringVal.getValS().startsWith(f)).findFirst();
+                    if (!forbidden.isPresent()) {
+                        if (stringVal.getValS().startsWith("w3pro_")) {
+                            stringVal.replaceBy(JassIm.ImStringVal(stringVal.getValS().substring(6)));
+                        } else {
+                            String text = stringVal.getValS();
+                            StringBuilder crypted = new StringBuilder();
+                            int i = 0;
+                            for (char c : text.toCharArray()) {
+                                if (c > 31 && c < 127) {
+                                    char shifted = (char)(c - ((char) key[i]));
+                                    if (shifted < 32) {
+                                        shifted += (128 - 32);
+                                    }
+                                    crypted.append(shifted);
+                                } else {
+                                    crypted.append(c);
                                 }
-                                crypted.append(shifted);
-                            } else {
-                                crypted.append(c);
+                                i++;
+                                if (i >= 4) {
+                                    i = 0;
+                                }
                             }
-                            i++;
-                            if (i >= 4) {
-                                i = 0;
-                            }
+                            stringVal.replaceBy(JassIm.ImFunctionCall(descryptFunction.attrTrace(), descryptFunction,
+                                JassIm.ImTypeArguments(), JassIm.ImExprs(JassIm.ImStringVal(crypted.toString())), true, CallType.NORMAL));
                         }
-                        stringVal.replaceBy(JassIm.ImFunctionCall(descryptFunction.attrTrace(), descryptFunction,
-                            JassIm.ImTypeArguments(), JassIm.ImExprs(JassIm.ImStringVal(crypted.toString())), true, CallType.NORMAL));
                     }
+
                 }
             });
         }
