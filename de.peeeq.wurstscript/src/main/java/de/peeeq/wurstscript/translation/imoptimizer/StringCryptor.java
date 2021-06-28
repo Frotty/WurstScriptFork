a796633b4119cbde4dffcbaba41a9147feb1c253
Frotty <Frotty@users.noreply.github.com>
2021-06-28 20:26:30 +0200

    Update StringCryptor.java

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/translation/imoptimizer/StringCryptor.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/translation/imoptimizer/StringCryptor.java
@@ -10,10 +10,12 @@ import de.peeeq.wurstscript.translation.imtranslation.ImHelper;
 import de.peeeq.wurstscript.translation.imtranslation.ImTranslator;
 import de.peeeq.wurstscript.utils.Utils;
 import de.peeeq.wurstscript.validation.TRVEHelper;
+import org.apache.commons.lang.math.RandomUtils;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -21,6 +23,7 @@ public class StringCryptor {
 
     public void encrypt(ImTranslator trans) {
         ImProg prog = trans.getImProg();
+        int[] key = new int[]{25, 12, 33, 17};
         Optional<ImFunction> decryptFunc = prog.getFunctions().stream().filter(f -> f.getName().contains("w3pro_decrypt_string")).findFirst();
         if (decryptFunc.isPresent()) {
             ImFunction descryptFunction = decryptFunc.get();
@@ -30,7 +33,26 @@ public class StringCryptor {
                     if (stringVal.getValS().startsWith("w3pro_")) {
                         stringVal.replaceBy(JassIm.ImStringVal(stringVal.getValS().substring(6)));
                     } else {
-                        stringVal.replaceBy(JassIm.ImFunctionCall(descryptFunction.attrTrace(), descryptFunction, JassIm.ImTypeArguments(), JassIm.ImExprs(stringVal.copy()), true, CallType.NORMAL));
+                        String text = stringVal.getValS();
+                        StringBuilder crypted = new StringBuilder();
+                        int i = 0;
+                        for (char c : text.toCharArray()) {
+                            if (c > 31 && c < 127) {
+                                char shifted = (char)(c - ((char) key[i]));
+                                if (shifted < 32) {
+                                    shifted += (128 - 32);
+                                }
+                                crypted.append(shifted);
+                            } else {
+                                crypted.append(c);
+                            }
+                            i++;
+                            if (i >= 4) {
+                                i = 0;
+                            }
+                        }
+                        stringVal.replaceBy(JassIm.ImFunctionCall(descryptFunction.attrTrace(), descryptFunction,
+                            JassIm.ImTypeArguments(), JassIm.ImExprs(JassIm.ImStringVal(crypted.toString())), true, CallType.NORMAL));
                     }
                 }
             });
